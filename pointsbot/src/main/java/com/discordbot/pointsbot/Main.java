8e8dd9a1f64b9a33e117362aeabfbcb7a09e5406
noahj13 <noahjarvis0@gmail.com>
2019-06-15 14:09:57 -0700

    Can give and take points to users

--- pointsbot/src/main/java/com/discordbot/pointsbot/Main.java
+++ pointsbot/src/main/java/com/discordbot/pointsbot/Main.java
@@ -2,19 +2,64 @@ package com.discordbot.pointsbot;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.user.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Map<User,Integer> pointTotals = new HashMap<>();
         // Insert your bot's token here
-        String token = "";
+        Scanner file = new Scanner(System.in);
+        try {
+            file = new Scanner(new File("pointsbot/Resources/token.cred"));
+        }catch (IOException e){
+            System.out.println("cannot find token");
+            System.exit(1);
+        }
+        String token = file.next();
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
         // Add a listener which answers with "Pong!" if someone writes "!ping"
         api.addMessageCreateListener(event -> {
-            if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
-                event.getChannel().sendMessage("Pong!");
+            String message = event.getMessageContent();
+            if (message.length() >= 5 && message.substring(0,5).equalsIgnoreCase("!give")){
+                for(User user: event.getMessage().getMentionedUsers()){
+                    if(pointTotals.get(user) != null)
+                        pointTotals.put(user,pointTotals.get(user)+1);
+                    else
+                        pointTotals.put(user,1);
+                }
+            }
+            else if (message.length() >= 5 && message.substring(0,5).equalsIgnoreCase("!take")){
+                for(User user: event.getMessage().getMentionedUsers()){
+                    pointTotals.put(user,pointTotals.get(user)+1);
+                }
+            }
+            else if (message.length() >= 7 && message.substring(0,7).equalsIgnoreCase("!points")){
+                String ret = "";
+                if (event.getMessage().getMentionedUsers() == null){
+                    ret += event.getMessageAuthor().getDiscriminatedName();
+                    if(pointTotals.get(event.getMessageAuthor().asUser()) == null){
+                        ret += ": 0";
+                    }
+                    else
+                        ret += ": " + pointTotals.get(event.getMessageAuthor().asUser());
+                    event.getChannel().sendMessage(ret);
+                }
+                else {
+                    for (User user : event.getMessage().getMentionedUsers()) {
+                        event.getChannel().sendMessage(user.getNicknameMentionTag() + ": " + pointTotals.get(user));
+                    }
+                }
+
             }
         });
 
